076f14ea5da582ed1e5690982a4f3dbea295ab8b
Michael Erb <Michael Erb>
2016-10-26 17:22:20 -0700

    players list and local storage are now synced

--- assets/js/angularApp.js
+++ assets/js/angularApp.js
@@ -1,30 +1,48 @@
-var app = angular.module('encounterTool', ['ngRoute']);
+var app = angular.module('encounterTool', ['ngRoute','ngStorage']);
 
-app.controller('PlayerController', ['$scope', '$http', function($scope, $http){
+app.controller('PlayerController', ['$scope', '$http', '$localStorage', function($scope, $http, $localStorage){
 
-  $scope.players = null;
-  $http.get('assets/js/data/players.json')
-      .success(function(data) {
-          $scope.players = data;
-      })
-      .error(function(data,status,error,config){
-          $scope.players = [{heading:"Error",description:"Could not load json data"}];
-      });
+
+
+  $scope.players = [];
+
+  var updatePlayers = function() {
+    if ($localStorage.players == undefined) {
+      $localStorage.players = [];
+    } else {
+      for (i = 0; i < $localStorage.players.length; i++) {
+        $scope.players.push($localStorage.players[i])
+      }
+    }
+  };
+
+  updatePlayers();
+
+  console.log('testing: '+$localStorage.platestyers);
+  console.log('testing: '+$localStorage.players);
+
 
   $scope.addNewPlayer = function(playerName, playerArmorClass, playerInitiative) {
-
     $scope.players.push(
       {
       	name: playerName,
       	ac: playerArmorClass,
       	initiative: playerInitiative
       }
-    )
+    );
+
+    $localStorage.players.push(
+      {
+      	name: playerName,
+      	ac: playerArmorClass,
+      	initiative: playerInitiative
+      }
+    );
+
     //clear out fields
     $scope.playerName = '';
     $scope.playerArmorClass = '';
     $scope.playerInitiative = '';
-
   };
 
 }]);

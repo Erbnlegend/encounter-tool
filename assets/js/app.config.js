3ab7f0565f9383065538ed6e2d980a0ab9c2f1f7
Erbnlegend <erbnlegend@gmail.com>
2016-10-29 16:50:27 -0700

    License Agreement

--- assets/js/app.config.js
+++ assets/js/app.config.js
@@ -1,13 +1,15 @@
 angular.module('encounterTool').
   config(['$locationProvider', '$routeProvider', '$localStorageProvider',
     function config($locationProvider, $routeProvider, $localStorageProvider) {
-      $locationProvider.hashPrefix('!');
       $localStorageProvider.setKeyPrefix('');
 
       $routeProvider.
         when('/', {
           templateUrl: './assets/js/views/players.html',
           controller: 'PlayerController'
+        }).
+        when('/license', {
+          templateUrl: './assets/js/views/license.html'
         }).
         otherwise('/');
     }

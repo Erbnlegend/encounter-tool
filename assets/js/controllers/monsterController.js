f74d6ba165d0aaedb90d93a342994cf84a14cdd6
Erbnlegend <erbnlegend@gmail.com>
2016-11-01 17:27:23 -0700

    Additional Styles and auto adjusting input fields

--- assets/js/controllers/monsterController.js
+++ assets/js/controllers/monsterController.js
@@ -79,9 +79,48 @@ app.controller('MonsterController', ['$scope', '$http', '$localStorage', functio
   // Needed so that changes are kept on change of initial monster creation
   // Fixes issue with updating local storage right after monster is created
     $scope.change = function(user, field) {
-      var index = $scope.user.indexOf(player);
+      var index = $scope.user.indexOf(user);
       $localStorage.user[index][field] = user[field];
     };
+
+// auto adjusting width for input values
+    $(function() {
+      function adjust(elements, offset, min, max) {
+        // initialize parameters
+        offset = offset || 0;
+        min    = min    || 0;
+        max    = max    || Infinity;
+        elements.each(function() {
+          var element = $(this);
+          // add element to measure pixel length of text
+          var id = btoa(Math.floor(Math.random() * Math.pow(2, 64)));
+          var tag = $('<span id="' + id + '">' + element.val() + '</span>').css({
+            'display': 'none',
+            'font-family': element.css('font-family'),
+            'font-size': element.css('font-size'),
+          }).appendTo('body');
+          // adjust element width on keydown
+          function update() {
+            // give browser time to add current letter
+            setTimeout(function() {
+              // prevent whitespace from being collapsed
+              tag.html(element.val().replace(/ /g, '&nbsp'));
+              // clamp length and prevent text from scrolling
+              var size = Math.max(min, Math.min(max, tag.width() + offset));
+              if (size < max)
+                element.scrollLeft(0);
+            // apply width to element
+            element.width(size);
+        }, 0);
+      };
+      update();
+      element.keydown(update);
+  });
+}
+// apply to our element
+  adjust($('.adjustText'), 20, 5, 500);
+  adjust($('.adjustNum'), 30, 5, 50);
+    });
 
 
 }]);
